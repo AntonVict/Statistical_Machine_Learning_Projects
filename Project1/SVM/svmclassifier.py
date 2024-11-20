1ab2c1fb43572e57a46b55b75ebefb49276ffc72
Anton Victorin <antonvic@kth.se>
2024-11-20 13:36:14 +0100

    kinda fixed the coding for part 1

--- Project1/SVM/svmclassifier.py
+++ Project1/SVM/svmclassifier.py
@@ -1,3 +1,5 @@
+import random
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
@@ -11,7 +13,7 @@ digits = datasets.load_digits()
 
 
 #choose a seed
-seed = XXXX
+seed = 42
 np.random.seed(seed)
 # Load data into train set and test set
 digits = datasets.load_digits()
@@ -19,12 +21,28 @@ X = digits.data
 y = np.array(digits.target, dtype = int)
 X,y = shuffle(X,y)
 N,d = X.shape
-Ntest = np.int(100)
-Ntrain = np.int(1697)
+Ntest = int(100)
+Ntrain = int(1697)
 Xtrain = X[0:Ntrain,:]
 ytrain = y[0:Ntrain]
 Xtest = X[Ntrain:N,:]
 ytest = y[Ntrain:N]
+
+
+#should we even compute the hinge-loss??
+#oohhhh this one should also return the states
+def multiClassHingeLoss(Theta, x, y):
+    margins = [] 
+    scores = []
+    numOfClasses = Theta.shape[1]
+    for j in range(numOfClasses):
+        # if j != y:
+        score = np.dot(x, (Theta[:,j]- Theta[:,y]))
+        margins.append(1 + score)
+            # scores.append(score)
+            
+    # margins.append()
+    return np.max(margins), margins
 
 
 def svmsubgradient(Theta, x, y):
@@ -33,9 +51,16 @@ def svmsubgradient(Theta, x, y):
 # The inputs are Theta, of size n-by-K, where K is the number of classes,
 # x of size n, and y an integer in {0, 1, ..., 9}.
     G = np.zeros(Theta.shape)
-    ## IMPLEMENT THE SUBGRADIENT CALCULATION -- YOUR CODE HERE
-
-    return(G)
+    
+    hingeLoss, margins = multiClassHingeLoss(Theta, x, y)
+    numOfClasses = Theta.shape[1]
+    if hingeLoss != 0:
+        for j in range(numOfClasses):
+            if j != y:
+                if margins[j] > 0:
+                    G[:, j] += x
+                    G[:, y] -= x
+    return G, hingeLoss
 
 def sgd(Xtrain, ytrain, maxiter = 10, init_stepsize = 1.0, l2_radius = 10000):
 #
@@ -58,8 +83,24 @@ def sgd(Xtrain, ytrain, maxiter = 10, init_stepsize = 1.0, l2_radius = 10000):
     mean_Theta = np.zeros(dd*K)
     mean_Theta.shape = dd,K
     ## YOUR CODE HERE -- IMPLEMENT PROJECTED STOCHASTIC GRADIENT DESCENT
+    
+    stepsize = init_stepsize/math.sqrt(K)
+    size_Dataset = Xtrain.shape[0]
+    loss_history = []
+    for _ in range(maxiter):
+        random_sample = random.randint(0, size_Dataset-1)
+        subGradient, loss = svmsubgradient(Theta, Xtrain[random_sample], ytrain[random_sample])
+        Theta -= stepsize*subGradient
 
-    return Theta, mean_Theta
+        frobenius_norm = np.linalg.norm(Theta, ord='fro')
+        if frobenius_norm >= l2_radius*2:
+            Theta = (l2_radius/frobenius_norm)*Theta
+        
+        loss_history.append(loss)
+        # projection set each value in theta to min(theta_ij, l2_radius)
+        # apparently the frobenius norm can be computed like this
+    
+    return Theta, mean_Theta, loss_history
 
 def Classify(Xdata, Theta):
 #
@@ -79,7 +120,26 @@ l2_radius = 40.0
 M_raw = np.sqrt(np.mean(np.sum(np.square(Xtrain))))
 init_stepsize = l2_radius/M_raw
 maxiter = 40000
-Theta, mean_Theta = sgd(Xtrain, ytrain, maxiter, init_stepsize, l2_radius)
+Theta, mean_Theta, loss_history = sgd(Xtrain, ytrain, maxiter, init_stepsize, l2_radius)
+
+# Plot the loss history
+plt.plot(loss_history)
+plt.xlabel('Iteration')
+plt.ylabel('Loss')
+plt.title('Loss History')
+plt.show()
+
+num_classes = Theta.shape[1]
+
+plt.figure(figsize=(10, 5))
+for i in range(num_classes):
+    plt.subplot(2, 5, i + 1)
+    plt.imshow(Theta[:, i].reshape(8, 8), cmap='gray')
+    plt.title(f' {i}')
+    plt.axis('off')
+
+plt.show()
+
 print('Error rate')
 print(np.sum(np.not_equal(Classify(Xtest, mean_Theta),ytest)/Ntest))
 
