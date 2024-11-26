662a00df342ad6b91de905000b6f6baec931272b
Axel Nilsson <123011574+axeln1lsson@users.noreply.github.com>
2024-11-26 22:59:21 +0100

    Corrected Part b)

--- Project1/GP/gpclassify.py
+++ Project1/GP/gpclassify.py
@@ -12,6 +12,9 @@ from sklearn.utils import shuffle
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.gaussian_process.kernels import RBF
 from sklearn.gaussian_process.kernels import DotProduct
+from sklearn.gaussian_process.kernels import ConstantKernel
+from sklearn.gaussian_process.kernels import WhiteKernel
+from sklearn.gaussian_process.kernels import Matern
 
 #choose a seed
 seed = 2
@@ -36,8 +39,10 @@ Xtest = X[Ntrain:N,:]
 ytest = y[Ntrain:N]
 
 
-#kernel = 1.0 * RBF([1.0]) # isotropic kernel
-kernel = DotProduct(1.0) 
+#kernel = ConstantKernel(1.0, (1e-2, 1e2)) * RBF(length_scale=10.0, length_scale_bounds=(1e-2, 1e2))
+kernel = ConstantKernel(1.0, (1e1, 1e3)) * Matern(length_scale=1.0, nu=1)
+
+#kernel = DotProduct(1.0) 
 #GaussianProcessClassifier performs hyperparameter estimation, this means that the the value specified above may not be the final hyperparameters
 #If you dont want it to do hyperparameter optimization set optimizer = None like this GaussianProcessClassifier(kernel=kernel,optimizer = None).fit(Xtrain, ytrain)
 #You can check the final hyperparameters with gpc_rbf.kernel_.get_params()['kernels']
@@ -51,4 +56,4 @@ test_error_rate = np.mean(np.not_equal(yp_test,ytest))
 print('Test error rate')
 print(test_error_rate)
 
-
+print("Optimized kernel:", gpc_rbf.kernel_, "\n")
